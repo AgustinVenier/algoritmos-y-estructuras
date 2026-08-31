d4ab855aa47ea786330d4971bb5a9a47f9930be6
AgustinVenier <agusvenier04@gmail.com>
2026-08-31 17:18:32 -0300

    Update pila

--- pila.c
+++ pila.c
@@ -1,9 +1,14 @@
 #include <stdio.h>
 #include <string.h>
+#include <stdlib.h>
+
+#define MIN(X,Y) ((X<Y)?(X):(Y))
 
 #define TAM 50
-#define ERROR 1;
-#define OK 0;
+#define PILA_LLENA -1
+#define PILA_VACIA -2
+#define OK 1
+
 typedef struct{
     char pila[TAM];
     unsigned tope;
@@ -18,6 +23,19 @@ int pilaLlena(const t_pila* pila, unsigned tam); // verifica si esta llena para 
 void vaciarPila(t_pila* pila);
 int main () {
     t_pila p1;
+    int elem = 15;
+    int elem_user;
+
+    crearPila(&p1);
+
+    apilar(&p1, &elem, sizeof(elem));
+    if(verTope(&p1, &elem_user, sizeof(elem_user))== PILA_VACIA)
+    {
+        printf("La pila esta vacia. No hay nada para ver...");
+        exit(1);
+    }
+    printf("%d\n", elem_user);
+
     return 0;
 }
 
@@ -29,24 +47,38 @@ void vaciarPila(t_pila* pila){
     pila->tope = TAM;
 }
 
-int pilaVacia(t_pila* pila){
+int pilaVacia(const t_pila* pila){
     if(pila->tope == TAM)
         return OK;
-    return ERROR;
+    return PILA_VACIA;
 
 }
 
-int pila
+int pilaLlena(const t_pila* pila, unsigned tam){
+    if(pila->tope < sizeof(tam) + tam)
+        return PILA_LLENA;
+    return OK;
+}
 
 int apilar(t_pila *pila, const void* elemento, unsigned tam){
-    int *ppila;
-    ppila = pila;
-    if(pila->tope - sizeof(tam) - tam >= 0)//verificamos que la pila tenga suficiente tamañano para meter el elemento y su tamaño
-        return ERROR;
 
-    memcpy((ppila) + (tam), elemento, tam);
-    memcpy((ppila) + (tam + sizeof(tam)), tam, sizeof(tam));
+    if(pila->tope < sizeof(tam) + tam)//verificamos que la pila tenga suficiente tamañano para meter el elemento y su tamaño
+        return PILA_LLENA;
+    pila->tope -= tam;
+    memcpy(pila->pila + pila->tope, elemento, tam);
 
-    if(pila->pila != NULL)
-        return OK;
+    pila->tope -= sizeof(tam);
+    memcpy(pila->pila + pila->tope, &tam, sizeof(tam));
+
+    return OK;
+}
+
+int verTope(const t_pila *pila, void* elemento, unsigned tam)
+{
+    unsigned tam_local;
+    if(pila->tope == TAM)
+        return PILA_VACIA;
+    memcpy(&tam_local, pila->pila + pila->tope, sizeof(unsigned));
+    memcpy(elemento, pila->pila + pila->tope + sizeof(unsigned), MIN(tam_local, tam));
+    return OK;
 }
